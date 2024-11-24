b60219c0e7e0d23e6b28f17086fcc449bf5526fe
AlanMuniz2772 <jorgeamr2772@gmail.com>
2024-11-24 17:23:20 -0600

    Bug: front-end url no adaptada para desarrollo

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://pos.hosthive.com.mx')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
 
     'allowed_origins_patterns' => [],
 
